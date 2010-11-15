9f9a4876f2113871abfaba025cb24f3f4ebd82a3
Tim <trliner@gmail.com>
2010-11-15 02:35:38 -0600

    setup initial stones

--- lib/pressman/board.rb
+++ lib/pressman/board.rb
@@ -1,19 +1,43 @@
 class Board
 
+  COLS = (1..8).collect{|n| :"col#{n}"}
+  ROWS = (1..8).collect{|n| :"row#{n}"}
+
   attr_accessor :grid
   attr_accessor :stones
 
   def initialize
     @grid = build_grid
-    @stones = {:black => 1, :white => 1}
+    @stones = {:black => 0, :white => 0}
+    self.place_initial_stones
   end
 
   def build_grid
-    col_hash = {}
-    (1..8).each {|n| col_hash[:"col#{n}"] = nil}
-    grid = {}
-    (1..8).each {|n| grid[:"row#{n}"] = col_hash}
-    grid
+    grid = Board::ROWS.inject({}) do |grid, row|
+      col_hash =  Board::COLS.inject({}) { |hash, col| hash.merge(col => nil) }
+      grid.merge(row =>col_hash )
+    end
+  end
+
+  def place_stone(row, col, color)
+    self.grid[row][col] = color
+    self.stones[color] +=1
+  end
+
+  def place_initial_stones
+    COLS.each do |col|
+      self.top_rows.each {|row| place_stone(row, col, :white)}
+      self.bottom_rows.each {|row| place_stone(row, col, :black)}
+    end
+  end
+
+  def top_rows
+    [ROWS[0], ROWS[1]]
+  end
+
+  def bottom_rows
+    length = ROWS.length
+    [ROWS[length - 1], ROWS[length - 2]]
   end
 
 end
--- spec/pressman/board_spec.rb
+++ spec/pressman/board_spec.rb
@@ -9,18 +9,45 @@ describe Board do
   end
 
   it "has eight rows" do
+    Board::ROWS.count.should == 8
     @board.grid.keys.count.should == 8
   end
 
   it "has eight columns" do
+    Board::COLS.count.should == 8
     @board.grid[:row1].keys.count.should == 8
   end
 
-  it "has a number of black stones" do
-    @board.stones[:black].should > 0
+end
+
+describe Board, "when first created" do
+
+  before do
+    @board = Board.new
   end
 
-  it "has a number of white stones" do
-    @board.stones[:white].should > 0
+  it "has sixteen black stones" do
+    @board.stones[:black].should == 16
   end
+
+  it "has sixteen white stones" do
+    @board.stones[:white].should == 16
+  end
+
+  it "has its top two rows filled with white stones" do
+    Board::COLS.each do |col|
+      @board.top_rows.each do |row|
+        @board.grid[row][col].should == :white
+      end
+    end
+  end
+
+  it "has its bottom two rows filled with black stones" do
+    Board::COLS.each do |col|
+      @board.bottom_rows.each do |row|
+        @board.grid[row][col].should == :black
+      end
+    end
+  end
+
 end
